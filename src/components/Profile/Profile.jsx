d5d0b4139d6cf54e32d4b029998214720cc8e4e7
saniyo <saniyo.ua@gmail.com>
2023-01-18 00:28:21 +0200

    again

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -20,7 +20,6 @@ export const User = ({
     stats: { followers, views, likes },
   },
 }) => {
-  {css};
   return (
     <>
       <UserCardContainer>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
 import { StatItem } from './StatItem';
-import styles from'./statistics.module.scss';
+import styles from './statistics.module.scss';
 
 export const Statistics = ({ title, items }) => {
-  const elements = items.map(item => {return (<StatItem key={item.id} {...item} />)});
+  const elements = items.map(item => {
+    return <StatItem key={item.id} {...item} />;
+  });
   return (
     <section className={styles.section}>
       {title && <h2 className={styles.header}>{title}</h2>}
